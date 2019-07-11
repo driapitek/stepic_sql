6eac82e72d60b1a2dffa5af13753d66fd72b530b
Stas Masyuta <41759392+driapitek@users.noreply.github.com>
2019-07-11 13:13:24 +0300

    solved 2 exercises for joining

--- exercise query/2week.sql
+++ exercise query/2week.sql
@@ -66,3 +66,57 @@ left outer join product as p on p.category_id = c.category_id;
 select * from product where price > 900
 union
 select * from product where price < 900;
+
+-- Выведите список товаров с названиями товаров и названиями категорий, в том числе товаров, не принадлежащих ни одной из категорий.
+select g.name as good_name, c.name as category_name  from good as g 
+	LEFT OUTER JOIN category_has_good as chg 
+		on g.id = chg.good_id
+	LEFT OUTER JOIN category as c
+		on c.id= chg.category_id
+order by good_name;
+
+-- Выведите список товаров с названиями категорий, в том числе товаров, не принадлежащих ни к одной из категорий, в том числе категорий не содержащих ни одного товара.
+
+select g.name as good_name, c.name as category_name  from good as g 
+	LEFT OUTER JOIN category_has_good as chg 
+		on g.id = chg.good_id
+	LEFT OUTER JOIN category as c
+		on c.id= chg.category_id
+ union       
+ select g.name as good_name, c.name as category_name  from good as g 
+	RIGHT OUTER JOIN category_has_good as chg 
+		on g.id = chg.good_id
+	RIGHT OUTER JOIN category as c
+		on c.id= chg.category_id;
+
+-- Выведите список всех источников клиентов и суммарный объем заказов по каждому источнику. Результат должен включать также записи для источников, по которым не было заказов. s.name as source_name, sum(sale.sale_sum) as sale_sum
+
+select s.name as source_name, sum(sale.sale_sum) as sale_sum from source as s 
+	LEFT OUTER JOIN client as c
+		on s.id = c.source_id
+	LEFT OUTER JOIN sale 
+		on c.id = sale.client_id
+group by source_name;
+
+-- Выведите названия товаров, которые относятся к категории 'Cakes' или фигурируют в заказах текущий статус которых 'delivering'. Результат не должен содержать одинаковых записей. В запросе необходимо использовать оператор UNION для объединения выборок по разным условиям.
+
+select  * from category_has_good;
+select	* from category;
+select	* from good;
+
+
+select good.name from good 
+	inner join category_has_good 
+		on good.id = category_has_good.good_id 
+	inner join category 
+		on category_has_good.category_id = category.id
+	where category.name = 'Cakes'
+union
+select good.name from good 
+	inner join sale_has_good 
+		on good.id = sale_has_good.good_id
+inner join sale 
+		on sale_has_good.sale_id = sale.id 
+inner join status 
+		on sale.status_id = status.id
+	where status.name = 'delivering';
